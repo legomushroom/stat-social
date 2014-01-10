084e4125f6ad153451ae91b4909009e1eb68f1cf
sol0mka <legomushroom@gmail.com>
2014-01-10 14:34:31 +0200

    working on double donut

--- js/vendor/pizza.js
+++ js/vendor/pizza.js
@@ -157,135 +157,189 @@
           start_angle = 0,
           base = $(this.identifier(legend)).width() - 4;
 
-      for (var i = 0; i < data.length; i++) {
-        total += data[i].value;
-      }
-
-      for (var i = 0; i < data.length; i++) {
-        angles[i] = data[i].value / total * Math.PI * 2;
-      }
-
-      if(angles.length == 1) angles[0] = Math.PI * 2 - 0.0001; // if 1
-
-      for (var i = 0; i < data.length; i++) {
-        var end_angle = start_angle + angles[i];
-        var cx = (base / 2),
-            cy = (base / 2),
-            r = ((base / 2) * 0.85);
-
-        if (!settings.donut) {
-          // Compute the two points where our wedge intersects the circle
-          // These formulas are chosen so that an angle of 0 is at 12 o'clock
-          // and positive angles increase clockwise
-          var x1 = cx + r * Math.sin(start_angle);
-          var y1 = cy - r * Math.cos(start_angle);
-          var x2 = cx + r * Math.sin(end_angle);
-          var y2 = cy - r * Math.cos(end_angle);
-
-          // This is a flag for angles larger than than a half circle
-          // It is required by the SVG arc drawing component
-          var big = 0;
-          if (end_angle - start_angle > Math.PI) big = 1;
-
-          // This string holds the path details
-          var d = "M" + cx + "," + cy +  // Start at circle center
-              " L" + x1 + "," + y1 +     // Draw line to (x1,y1)
-              " A" + r + "," + r +       // Draw an arc of radius r
-              " 0 " + big + " 1 " +      // Arc details...
-              x2 + "," + y2 +            // Arc goes to to (x2,y2)
-              " Z";                      // Close path back to (cx,cy)
+      if (settings.mode !== 'circle'){
+        for (var i = 0; i < data.length; i++) {
+          total += data[i].value;
         }
 
-        var existing_path = $('path[data-id="s' + i + '"]', svg.node);
-
-        if (existing_path.length > 0) {
-          var path = Snap(existing_path[0]);
-        } else {
-          var path = svg.path();
+        for (var i = 0; i < data.length; i++) {
+          angles[i] = data[i].value / total * Math.PI * 2;
         }
 
-        var percent = (data[i].value / total) * 100.0;
+        if(angles.length == 1) angles[0] = Math.PI * 2 - 0.0001; // if 1
 
-        // thanks to Raphael.js
-        var existing_text = $('text[data-id="s' + i + '"]', svg.node);
+        for (var i = 0; i < data.length; i++) {
+          var end_angle = start_angle + angles[i];
+          var cx = (base / 2),
+              cy = (base / 2),
+              r = ((base / 2) * 0.85);
 
-        if (existing_text.length > 0) {
-          var text = Snap(existing_text[0]);
+          if (!settings.donut) {
+            // Compute the two points where our wedge intersects the circle
+            // These formulas are chosen so that an angle of 0 is at 12 o'clock
+            // and positive angles increase clockwise
+            var x1 = cx + r * Math.sin(start_angle);
+            var y1 = cy - r * Math.cos(start_angle);
+            var x2 = cx + r * Math.sin(end_angle);
+            var y2 = cy - r * Math.cos(end_angle);
 
-          text.attr({
-            x: cx + (r + settings.percent_offset) * Math.sin(start_angle + (angles[i] / 2)),
-            y: cy - (r + settings.percent_offset) * Math.cos(start_angle + (angles[i] / 2))
-          });
+            // This is a flag for angles larger than than a half circle
+            // It is required by the SVG arc drawing component
+            var big = 0;
+            if (end_angle - start_angle > Math.PI) big = 1;
 
-        } else {
-
-          if (data[i].text) {
-            var visible_text = this.parse_options(data[i].text, percent, data[i].value);
-          } else {
-            var visible_text = Math.ceil(percent) + '%';
+            // This string holds the path details
+            var d = "M" + cx + "," + cy +  // Start at circle center
+                " L" + x1 + "," + y1 +     // Draw line to (x1,y1)
+                " A" + r + "," + r +       // Draw an arc of radius r
+                " 0 " + big + " 1 " +      // Arc details...
+                x2 + "," + y2 +            // Arc goes to to (x2,y2)
+                " Z";                      // Close path back to (cx,cy)
           }
-          var text = path.paper.text(cx + (r + settings.percent_offset) * Math.sin(start_angle + (angles[i] / 2)), cy - (r + settings.percent_offset) * Math.cos(start_angle + (angles[i] / 2)), visible_text);
-        }
 
-        var xCenter = cx + (r - settings.percent_offset) * Math.sin(start_angle + (angles[i] / 2));
-        var yCenter = cy - (r - settings.percent_offset) * Math.cos(start_angle + (angles[i] / 2));
+          var existing_path = $('path[data-id="s' + i + '"]', svg.node);
 
-        var left_offset = text.getBBox().width / 2;
+          if (existing_path.length > 0) {
+            var path = Snap(existing_path[0]);
+          } else {
+            var path = svg.path();
+          }
 
-        if (settings.always_show_text) {
-          text.attr({
-            x: text.attr('x') - left_offset,
-            opacity: 1
+          var percent = (data[i].value / total) * 100.0;
+
+          // thanks to Raphael.js
+          var existing_text = $('text[data-id="s' + i + '"]', svg.node);
+
+          if (existing_text.length > 0) {
+            var text = Snap(existing_text[0]);
+
+            text.attr({
+              x: cx + (r + settings.percent_offset) * Math.sin(start_angle + (angles[i] / 2)),
+              y: cy - (r + settings.percent_offset) * Math.cos(start_angle + (angles[i] / 2))
+            });
+
+          } else {
+
+            if (data[i].text) {
+              var visible_text = this.parse_options(data[i].text, percent, data[i].value);
+            } else {
+              var visible_text = Math.ceil(percent) + '%';
+            }
+            var text = path.paper.text(cx + (r + settings.percent_offset) * Math.sin(start_angle + (angles[i] / 2)), cy - (r + settings.percent_offset) * Math.cos(start_angle + (angles[i] / 2)), visible_text);
+          }
+
+          var xCenter = cx + (r - settings.percent_offset) * Math.sin(start_angle + (angles[i] / 2));
+          var yCenter = cy - (r - settings.percent_offset) * Math.cos(start_angle + (angles[i] / 2));
+
+          var left_offset = text.getBBox().width / 2;
+
+          if (settings.always_show_text) {
+            text.attr({
+              x: text.attr('x') - left_offset,
+              opacity: 1
+            });
+          } else {
+            text.attr({
+              x: text.attr('x') - left_offset,
+              opacity: 0
+            });
+          }
+
+          text.node.setAttribute('data-id', 's' + i);
+          path.node.setAttribute('data-cx', cx);
+          path.node.setAttribute('data-cy', cy);
+
+          if (settings.donut) {
+            this.annular_sector(path.node, {
+              centerX:cx, centerY:cy,
+              startDegrees:start_angle, endDegrees:end_angle,
+              innerRadius: (r * settings.donut_inner_ratio), outerRadius:r
+            });
+
+          } else {
+            path.attr({d:d});
+          }
+
+          path.attr({
+            fill: data[i].color,
+            stroke: settings.stroke_color,
+            strokeWidth: settings.stroke_width
           });
-        } else {
-          text.attr({
-            x: text.attr('x') - left_offset,
-            opacity: 0
-          });
+
+          path.node.setAttribute('data-id', 's' + i);
+
+          this.animate(path, cx, cy, settings);
+
+          // The next wedge begins where this one ends
+          start_angle = end_angle;
+
+          if (settings.mode === 'donut-path'){
+            self = this;
+            (function (i, settings, svg, cx, cy, xCenter, yCenter, visible_text, r) {
+              setTimeout(function () {
+                self.drawDonutPath.apply(self,[i,settings, svg, cx, cy, xCenter, yCenter, visible_text, r])  
+              }, 1); 
+            })(i, settings, svg, cx, cy, xCenter, yCenter, visible_text, r);
+          }
+
         }
+      // MODE IS CIRCLE
+      } else{
+        var r = ((base / 2) * 0.85);
 
-        text.node.setAttribute('data-id', 's' + i);
-        path.node.setAttribute('data-cx', cx);
-        path.node.setAttribute('data-cy', cy);
-
-        if (settings.donut) {
-          this.annular_sector(path.node, {
-            centerX:cx, centerY:cy,
-            startDegrees:start_angle, endDegrees:end_angle,
-            innerRadius: (r * settings.donut_inner_ratio), outerRadius:r
-          });
-
-        } else {
-          path.attr({d:d});
-        }
-
-        path.attr({
-          fill: data[i].color,
-          stroke: settings.stroke_color,
-          strokeWidth: settings.stroke_width
+        var baseCircle = this.checkExistence({
+          svg: svg,
+          id: 'base-circle'
+          // type: 'circle'
         });
 
-        path.node.setAttribute('data-id', 's' + i);
+        baseCircle.attr({
+          fill: data[0].color,
+          r: r
+        });
 
-        this.animate(path, cx, cy, settings);
+        this.annular_sector(baseCircle.node, {
+          centerX:base/2, centerY:base/2,
+          startDegrees:0, endDegrees:31.4155,
+          innerRadius: (r*.35), outerRadius:r
+        });
 
-        // The next wedge begins where this one ends
-        start_angle = end_angle;
 
-        self = this;
-        (function (i, settings, svg, cx, cy, xCenter, yCenter, visible_text, r) {
-          setTimeout(function () {
-            self.drawDonutPath.apply(self,[i,settings, svg, cx, cy, xCenter, yCenter, visible_text, r])  
-          }, 1); 
-        })(i, settings, svg, cx, cy, xCenter, yCenter, visible_text, r);
+        var innerCircle = this.checkExistence({
+          svg: svg,
+          id: 'inner-circle'
+        });
+
+        innerCircle.attr({
+          fill: data[1].color,
+          r: r/2
+        });
+
+        var percent = data[1].value;
+
+        var holePercent = 35/100;
+        var circleHole = (r*holePercent);
+        var invertedGole = 1-holePercent;
+        percent *= invertedGole;
+        var progress = r*(percent/100);
+
+
+        this.annular_sector(innerCircle.node, {
+          centerX:base/2, centerY:base/2,
+          startDegrees:0, endDegrees:31.4155,
+          innerRadius: circleHole, outerRadius:circleHole + progress
+        });
+
 
       }
+
+      
 
       return [legend, svg.node];
     },
 
     drawDonutPath: function (i, settings, svg, cx, cy, xCenter, yCenter, visible_text, r) {
-      if (settings.mode === 'donut-path'){
+      
         var paperWidth = parseInt(svg.attr('width'),10); 
 
         if (i === 0){
@@ -546,8 +600,6 @@
 
         }
 
-        }
-
     },
 
     animate : function (el, cx, cy, settings) {
@@ -605,7 +657,7 @@
         svg = Snap(width, height);
       }
 
-      var offsetX = (settings.mode === 'donut-path') ? this.DONUT_PATH_OFFSET : 0;
+      var offsetX = ((settings.mode === 'donut-path') || (settings.mode === 'circle')) ? this.DONUT_PATH_OFFSET : 0;
 
 
       svg.node.setAttribute('width', width + settings.percent_offset + offsetX);
